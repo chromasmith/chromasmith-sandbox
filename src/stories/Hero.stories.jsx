1cbd2b558bc8229f2a83f4b293989f65e86ae771
chromasmith <chromasmith1@gmail.com>
2025-10-21 07:32:50 -0700

    Add Hero stories

---
+++ src/stories/Hero.stories.jsx
@@ -0,0 +1,9 @@
+import { Hero } from '../components/Hero.jsx';
+
+export const Default = () => (
+  <Hero title="Welcome to Our Site" subtitle="Building amazing experiences" />
+);
+
+export const CustomText = () => (
+  <Hero title="Get Started" subtitle="Your journey begins here" />
+);
