fef657d894a8ec9bff8805e4341297b02ec2360f
chromasmith <chromasmith1@gmail.com>
2025-11-02 08:36:35 -0800

    feat(providers): Register MySQL provider in index

--- .forge/lib/providers/index.cjs
+++ .forge/lib/providers/index.cjs
@@ -6,16 +6,19 @@
 const { registry } = require('./provider-registry.cjs');
 const { SupabaseProvider } = require('./provider-supabase.cjs');
 const { FirebaseProvider } = require('./provider-firebase.cjs');
+const { MySQLProvider } = require('./provider-mysql.cjs');
 
 // Register providers
 registry.register('supabase', SupabaseProvider);
 registry.register('firebase', FirebaseProvider);
+registry.register('mysql', MySQLProvider);
 
 // Export registry for use
 module.exports = {
   registry,
   providers: {
     SupabaseProvider,
-    FirebaseProvider
+    FirebaseProvider,
+    MySQLProvider
   }
 };
