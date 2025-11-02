9119c93195032428b87abe211a55047d7a49b09b
chromasmith <chromasmith1@gmail.com>
2025-11-02 08:57:17 -0800

    feat(providers): Register DynamoDB provider in registry - completes provider matrix

--- .forge/lib/providers/index.cjs
+++ .forge/lib/providers/index.cjs
@@ -8,12 +8,14 @@ const { SupabaseProvider } = require('./provider-supabase.cjs');
 const { FirebaseProvider } = require('./provider-firebase.cjs');
 const { MySQLProvider } = require('./provider-mysql.cjs');
 const { MongoDBProvider } = require('./provider-mongodb.cjs');
+const { DynamoDBProvider } = require('./provider-dynamodb.cjs');
 
 // Register providers
 registry.register('supabase', SupabaseProvider);
 registry.register('firebase', FirebaseProvider);
 registry.register('mysql', MySQLProvider);
 registry.register('mongodb', MongoDBProvider);
+registry.register('dynamodb', DynamoDBProvider);
 
 // Export registry for use
 module.exports = {
@@ -22,6 +24,7 @@ module.exports = {
     SupabaseProvider,
     FirebaseProvider,
     MySQLProvider,
-    MongoDBProvider
+    MongoDBProvider,
+    DynamoDBProvider
   }
 };
